f2db548a1d78295a6bd41ec79c855d95809605a1
Raul Rohjans <rauljann@hotmail.com>
2024-08-01 22:54:39 +0100

    Removed unsued import

--- scraper.py
+++ scraper.py
@@ -6,7 +6,6 @@ Provides all the utils to scrape the data
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
